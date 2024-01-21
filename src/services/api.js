1d29762901409ed9f1ce71c64b80bfad7d885302
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2024-01-21 16:57:28 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://voiturecloud-production-df74.up.railway.app/', // Remplacez par l'URL de votre backend
+  baseURL: 'https://voiturecloud-production-171b.up.railway.app/', // Remplacez par l'URL de votre backend
   headers: {
     'Content-Type': 'application/json',
   },
